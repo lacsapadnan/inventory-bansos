f46c52df430a975a7c7aa16d79730ae5646d6124
lacsapadnan <pascaladnan101@gmail.com>
2021-12-12 22:19:56 +0700

    Update logo

--- resources/views/parts/navbar.blade.php
+++ resources/views/parts/navbar.blade.php
@@ -1,8 +1,8 @@
 <div class="nav-header">
     <a href="#" class="brand-logo">
-        <img class="logo-abbr" src="{{ URL::asset('assets/images/logo.png') }}" alt="">
-        <img class="logo-compact" src="{{ URL::asset('assets/images/logo-text.png') }}" alt="">
-        <img class="brand-title" src="{{ URL::asset('assets/images/logo-text.png') }}" alt="">
+        {{-- <img class="logo-abbr" src="{{ URL::asset('assets/images/logo-1.png') }}" alt=""> --}}
+        <img class="logo-compact" src="{{ URL::asset('assets/images/logo-1.png') }}" alt="">
+        <img class="brand-title" src="{{ URL::asset('assets/images/logo-1.png') }}" alt="">
     </a>
 
     <div class="nav-control">
